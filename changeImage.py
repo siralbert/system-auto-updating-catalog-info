4b17369273fe5f0c27f6599f1d6e0bbc82b2b1bf
Albert <albert@DESKTOP-N71VLB4.attlocal.net>
2024-01-04 09:56:21 -0600

    Add convert_image and convert_images methods to changeImage.py module and create unit tests.

---
+++ changeImage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import re
+import glob, os
+
+#import subprocess
+#from multiprocessing import Pool
+
+# modifies image attributes (format, size, and rotation)
+# (dest_path includes filename and extension type of source file)
+def convert_image(path, dest_path, ext='.jpeg', size=(128,128), rotation=270):
+  im = Image.open(path)
+  new_dest_path = re.sub(r"\.\w{1,4}$",'.jpeg',dest_path)
+  im.rotate(rotation).resize(size).convert("RGB").save(new_dest_path)
+  im.close()
+
+
+# retrieves Image attributes from file
+def view_image_attr(path):
+  im = Image.open(path)
+  size = im.size
+  mode = im.mode
+  fmt = im.format
+  im.close()
+  return {'size':size,'mode':mode,'format':fmt}
+
+# process all images in current directory
+def convert_images(path,size=(128,128),rotation=0):
+  size = 600, 400
+  for infile in glob.glob(os.path.join(path,"*.tiff")):
+    file, ext = os.path.splitext(infile)
+    with Image.open(infile) as im:
+      destfile = file + '.jpeg'
+      convert_image(file + ext,destfile, size=size,rotation=rotation)
+
+if __name__ == "__main__":
+#    spawn_thread()
+#   TEST CASE
+  convert_image('supplier-data/images/sample.tif','supplier-data/images/tiff-image.jpeg')
+  convert_images('supplier-data/images')
+
--- tests/test_changeImage.py
+++ tests/test_changeImage.py
@@ -8,8 +8,45 @@ from changeImage import *
 
 class TestChangeImage(unittest.TestCase):
   def test_basic(self):
-    testcase = ""
-    expected = ""
+    testcase = {'src_dir'  : 'supplier-data/images',
+                'dest_dir' : 'supplier-data/images'}
+    expected = {'src_dir'  : 'supplier-data/images',
+                'dest_dir' : 'supplier-data/images'}
     self.assertEqual(testcase,expected)
+
+  def test_convert_images(self):
+    testcase = {'src_dir'  : 'supplier-data/images',
+                'dest_dir' : 'supplier-data/images',
+                'size' : (600,400),
+                'rotation' : 0}
+    expected =  {
+                 "size" : (600,400),
+                 "mode" : 'RGB',
+                 "format" : 'JPEG',
+                } 
+    convert_images(testcase['src_dir'],size=testcase['size'],rotation=testcase['rotation'])
+    for infile in glob.glob(os.path.join(testcase['dest_dir'],'*.jpeg')):
+        image_attr = view_image_attr(infile)
+        self.assertEqual(image_attr['size'],expected['size'])
+        self.assertEqual(image_attr['mode'],expected['mode'])
+        self.assertEqual(image_attr['format'],expected['format'])
+
+  def test_convert_image(self):
+    testcase = {'src_path'  : 'supplier-data/images/sample.tif',
+                  'dest_path' : 'supplier-data/images/sample.jpeg'}
+    expected =  {
+                 "size" : (600,400),
+                 "mode" : 'RGB',
+                 "format" : 'JPEG',
+                } 
+    convert_image(testcase['src_path'],testcase['dest_path'],size=(600,400))
+    image_attr = view_image_attr(testcase['dest_path'])
+    self.assertEqual(image_attr['size'],expected['size'])
+    self.assertEqual(image_attr['mode'],expected['mode'])
+    self.assertEqual(image_attr['format'],expected['format'])
+
+  def test_view_image_attr(self):
+    testcase = 'supplier-data/images/sample.tif'
+    view_image_attr(testcase)
 
 unittest.main()
