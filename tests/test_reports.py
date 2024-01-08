0a046acf63634e98824a32e8df41205481d8fd88
Albert <albert@DESKTOP-N71VLB4.attlocal.net>
2024-01-08 07:36:57 -0600

    Add initial methods for reports along with basic unit test framework

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 
 import unittest
+from unittest.mock import MagicMock
 import sys
 sys.path.append("..")
 
 from reports import *
+
+class TestReports(unittest.TestCase):
+  
+  def setUp(self):
+    pass
+
+  def test_basic(self):
+    testcase = "" 
+    expected = "" 
+    self.assertEqual(testcase,expected)
+
+  def test_generate(self):
+    testcase = "" 
+    expected = "" 
+    self.assertEqual(testcase,expected)
+  
+  def tearDown(self):
+    pass
+
+unittest.main()
+
